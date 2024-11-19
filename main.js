ac7afa455c90c38c54f5591d2faf525ea1e7cc3b
liobrasil <limol.lionel@gmail.com>
2024-11-18 21:30:03 -0400

    fix(main.js): correct logging of balance difference by replacing toBalanceDifference with fromBalanceDifference to ensure accurate output

--- main.js
+++ main.js
@@ -1889,7 +1889,7 @@ async function processBlockTransactions(blockNumber) {
         "Profit in USD:",
         toBalanceDifference,
         " || ",
-        toBalanceDifference
+        fromBalanceDifference
       );
       console.log("Number of swaps:", swapEventCount);
       console.log("Dex path:", dexPath);
