1ee1c3de425b21cc058928dd9bce67151ec3de40
liobrasil <limol.lionel@gmail.com>
2024-12-03 13:50:09 -0400

    fix(main.js): update token symbol handling from WBNB to WFTM to reflect correct mapping in price calculations refactor(main.js): adjust transaction fee calculations to use FTM instead of BNB for accurate USD conversion

--- main.js
+++ main.js
@@ -2835,8 +2835,8 @@ async function fetchAllPrices() {
 async function addUsdAmounts(tokens, priceMap) {
   // Map over the transactions to add the usdAmount field
   return tokens.map((token) => {
-    // Handle special case for WBNB -> BNB
-    token.symbol === "WBNB" && (token.symbol = "BNB");
+    // Handle special case for WFTM -> FTM
+    token.symbol === "WFTM" && (token.symbol = "FTM");
     const symbolToUSDT = `${token.symbol}-USDT`;
     const symbolToUSDC = `${token.symbol}-USDC`;
     const symbolToUSD = `${token.symbol}-USD`;
@@ -3023,7 +3023,7 @@ async function processBlockTransactions(blockNumber) {
     if (gasUsed) {
       txnFees = Number(gasUsed) * Number(ethers.formatEther(gasPrice));
     }
-    let txnFeesUsd = txnFees * priceMap["BNB-USDT"];
+    let txnFeesUsd = txnFees * priceMap["FTM-USDT"];
 
     if (!hasSwapEvent) continue;
 
@@ -3065,8 +3065,8 @@ async function processBlockTransactions(blockNumber) {
       let amountInRate = tokenPath[0].split("=>")[0].includes("USD")
         ? 1
         : priceMap[
-            tokenPath[0].split("=>")[0] === "WBNB"
-              ? "BNB-USDT"
+            tokenPath[0].split("=>")[0] === "WFTM"
+              ? "FTM-USDT"
               : tokenPath[0].split("=>")[0] + "-USDT"
           ];
 
@@ -3075,8 +3075,8 @@ async function processBlockTransactions(blockNumber) {
         .includes("USD")
         ? 1
         : priceMap[
-            tokenPath[tokenPath.length - 1].split("=>")[1] === "WBNB"
-              ? "BNB-USDT"
+            tokenPath[tokenPath.length - 1].split("=>")[1] === "WFTM"
+              ? "FTM-USDT"
               : tokenPath[tokenPath.length - 1].split("=>")[1] + "-USDT"
           ];
 
